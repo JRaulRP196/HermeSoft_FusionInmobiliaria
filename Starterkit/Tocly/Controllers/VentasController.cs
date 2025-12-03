3c1cc759d1b844e8ef50019e1d17c54e6151568e
Michelle <105686334+Micheller24@users.noreply.github.com>
2025-12-02 20:15:21 -0600

    Modulo de ventas

---
+++ Starterkit/Tocly/Controllers/VentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HermeSoft_Fusion.Controllers
+{
+    public class VentasController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Registro() => View();
+        public IActionResult Detalle() => View();
+        public IActionResult Editar() => View();
+    }
+}
