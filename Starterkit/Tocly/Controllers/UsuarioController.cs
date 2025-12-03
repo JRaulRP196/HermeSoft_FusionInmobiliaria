1d45d45be20236e9d853684ecf224d5ed1e49eaa
Jan Quesada <janjosueq@gmail.com>
2025-12-02 22:42:09 -0600

    vistaUsuarios
    se agrego la vista de los usuarios

---
+++ Starterkit/Tocly/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using HermeSoft_Fusion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HermeSoft_Fusion.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
