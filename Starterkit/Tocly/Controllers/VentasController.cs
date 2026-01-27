a7ab5fc6169d4fe8f716e716082105a41e0c604b
Jose Raúl Rodríguez <rodriguezpaniaguaj@gmail.com>
2026-01-27 10:29:50 -0600

    CalculoDesglosePrima
    Pendiente de ver como hacerlo en controller (Backend)

--- Starterkit/Tocly/Controllers/VentasController.cs
+++ Starterkit/Tocly/Controllers/VentasController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using HermeSoft_Fusion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace HermeSoft_Fusion.Controllers
 {
     public class VentasController : Controller
     {
+
+        private HttpClient _httpClient;
+
+        public VentasController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
         public IActionResult Index() => View();
-        public IActionResult Registro() => View();
+        public async Task<IActionResult> Registro(string lote)
+        {
+            ViewBag.lote = lote;
+            return View();
+        }
+
         public IActionResult Detalle() => View();
         public IActionResult Editar() => View();
+
+        #region Utilidades
+        [HttpGet]
+        public async Task<Lote> ObtenerLote(string lote)
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:3000/lotes/{lote}");
+            var json = await response.Content.ReadAsStringAsync();
+
+            var lotes = JsonSerializer.Deserialize<List<Lote>>(json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+            var lot = lotes.FirstOrDefault();
+            return lot;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerLoteJson(string lote)
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:3000/lotes/{lote}");
+            var json = await response.Content.ReadAsStringAsync();
+
+            return Content(json, "application/json");
+        }
+        #endregion
     }
 }
---
+++ Starterkit/Tocly/wwwroot/assets/js/calculos.js
@@ -0,0 +1,68 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const idLote = $("#lote");
+    const porcentajePrima = $("#porcPrima");
+    const fechaVencimientoPrima = $("#fechaPrima");
+    const resultadoPrima = $("#desglosePrima");
+
+    //Para el calculo de prima automatico
+
+    function ObtenerLote(codigoLote) {
+        return fetch(`/Ventas/ObtenerLoteJson?lote=${codigoLote}`)
+            .then(res => res.json());
+    }
+    async function calcularPrima() {
+        const lote = await ObtenerLote(idLote.val());
+        const inicio = new Date();
+        const fin = new Date(fechaVencimientoPrima.val());
+        const porcentaje = porcentajePrima.val() * 0.01;
+
+        let meses = (fin.getFullYear() - inicio.getFullYear()) * 12 +
+                    (fin.getMonth() - inicio.getMonth());
+
+        if (fin.getDate() < inicio.getDate()) {
+            meses--;
+        }
+
+        const montoPrima = lote[0].precioVenta * porcentaje;
+        const mensualidad = montoPrima / meses;
+        console.log("mensualidad: " + mensualidad + " meses: " + meses + " montoPrima: " + montoPrima);
+
+    }
+
+    function datosCompletosParaPrima() {
+
+        if (!idLote.val()) return false;
+        if (!fechaVencimientoPrima.val()) return false;
+        if (!porcentajePrima.val()) return false;
+
+        const hoy = new Date();
+        const fin = new Date(fechaVencimientoPrima.val());
+
+        if (fin <= hoy) {
+            return false;
+        } else if (fin.getMonth() == hoy.getMonth()) {
+            if (fin.getFullYear() == hoy.getFullYear()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function actualizarBotonPrima() {
+        if (datosCompletosParaPrima()) {
+            $("#calcPrima").removeClass("d-none");
+        } else {
+            $("#calcPrima").addClass("d-none");
+        }
+    }
+
+    fechaVencimientoPrima.on("change", actualizarBotonPrima);
+    porcentajePrima.on("change keyup", actualizarBotonPrima);
+
+    $("#calcPrima").on("click", function () {
+        calcularPrima();
+    });
+
+});
