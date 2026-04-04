883c58e2821f69249db357700e73b6f08f1a4090
Jan Quesada <janjosueq@gmail.com>
2026-04-03 19:50:37 -0600

    Muestra detalle y atributos adicionales de primas

---
+++ Hermesoft/Hermesoft/Controllers/PrimaController.cs
@@ -0,0 +1,27 @@
+﻿using HermeSoft_Fusion.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HermeSoft_Fusion.Controllers
+{
+    public class PrimaController : Controller
+    {
+        private readonly PrimaBusiness _primaBusiness;
+        public PrimaController(PrimaBusiness primaBusiness)
+        {
+            _primaBusiness = primaBusiness;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Detalle(int idPrima)
+        {
+            var prima = await _primaBusiness.ObtenerPorId(idPrima);
+
+            if (prima == null)
+            {
+                TempData["MensajeError"] = "No se encontró la prima solicitada.";
+                return RedirectToAction("Primas", "Estadistica");
+            }
+
+            return View(prima);
+        }
+    }
+}
